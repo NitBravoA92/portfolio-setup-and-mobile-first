d91f4576e9b9f37e6b24a2d9842279a248d26ce8
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-05-15 19:34:03 -0400

    Update the js code of the mobile menu

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const menuBtn = document.getElementById("open-mobile-menu");
 const menu = document.getElementById('mobile-menu');
 const btnClose = document.getElementById('close-btn');
+const menuLinks = document.querySelectorAll('.mobile-nav-link');
 
 function navToggle() {
     menu.classList.remove('hide-menu')
-    menu.classList.toggle('show-menu');
-    menuBtn.classList.toggle('open');
+    menu.classList.add('show-menu');
+    menuBtn.classList.add('open');
 }
 
 function closeMobileNav() {
-    menu.classList.add('hide-menu')
-    menu.classList.remove('show-menu')
+    menu.classList.add('hide-menu');
+    menu.classList.remove('show-menu');
+    menuBtn.classList.remove('open');
 }
 
 btnClose.addEventListener('click', closeMobileNav)
 menuBtn.addEventListener('click', navToggle)
+menuLinks.forEach(navLink => {
+    navLink.addEventListener('click', closeMobileNav);
+})
