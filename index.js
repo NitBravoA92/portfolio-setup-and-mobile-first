6be6713cdc828d121e611f63566fcc2dc2ea4bd2
kenagy100@gmail.com <kenagy100@gmail.com>
2023-05-18 16:20:17 +0000

    Add storage function to javascript

--- index.js
+++ index.js
@@ -179,3 +179,22 @@ contactForm.addEventListener('submit', (event) => {
     errorMessage.style.display = 'block';
   }
 });
+
+// Local Storage
+function storage(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return e instanceof DOMException && (
+      e.code === 22
+      || e.code === 1014
+      || e.name === 'QuotaExceededError'
+      || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
+      && (storage && storage.length !== 0);
+  }
+}
